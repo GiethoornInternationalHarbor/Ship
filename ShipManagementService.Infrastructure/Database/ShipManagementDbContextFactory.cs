a1509f8460375f6cf4f323e2a8f4a9dd656d1675
DanielvanBavel[user] <daniel.ajax.1996@hotmail.com>
2018-05-22 21:46:33 +0200

    adding migrations

--- ShipManagementService.Infrastructure/Database/ShipManagementDbContextFactory.cs
+++ ShipManagementService.Infrastructure/Database/ShipManagementDbContextFactory.cs
@@ -4,9 +4,6 @@ namespace ShipManagementService.Infrastructure.Database
 {
     public class ShipManagementDbContextFactory 
     {        
-        /// <summary>
-        /// Gets or sets the connection string.
-        /// </summary>
         protected string ConnectionString { get; set; }
 
         public ShipManagementDbContextFactory(string connectionString)
@@ -14,10 +11,6 @@ namespace ShipManagementService.Infrastructure.Database
             ConnectionString = connectionString;
         }
 
-        /// <summary>
-        /// Creates the database context.
-        /// </summary>
-        /// <returns></returns>
         public ShipManagementDbContext CreateDbContext()
         {
             var optBuilder = new DbContextOptionsBuilder<ShipManagementDbContext>();
---
+++ ShipManagementService.Infrastructure/Migrations/20180522192119_InitialCreate.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace ShipManagementService.Infrastructure.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Customers",
+                columns: table => new
+                {
+                    Email = table.Column<string>(nullable: false),
+                    Address = table.Column<string>(nullable: false),
+                    PostalCode = table.Column<string>(nullable: false),
+                    Residence = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Customers", x => x.Email);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Ship",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Email = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Ship", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Service",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Name = table.Column<string>(nullable: false),
+                    Price = table.Column<double>(nullable: false),
+                    ShipId = table.Column<Guid>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Service", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Service_Ship_ShipId",
+                        column: x => x.ShipId,
+                        principalTable: "Ship",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Service_ShipId",
+                table: "Service",
+                column: "ShipId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Customers");
+
+            migrationBuilder.DropTable(
+                name: "Service");
+
+            migrationBuilder.DropTable(
+                name: "Ship");
+        }
+    }
+}
